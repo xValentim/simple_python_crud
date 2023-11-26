4b42a316915b1fbb7163a750d68fd9638c55934c
xValentim <joaogvr@al.insper.edu.br>
2023-11-26 16:24:19 -0300

    add watch

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import boto3
 import os
-
+import asyncio
+import logging
+import time
+from botocore.exceptions import ClientError, NoCredentialsError
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
@@ -19,6 +24,69 @@ if database_url is None:
 engine = create_engine(database_url)
 Base = declarative_base()
 
+async def get_secret():
+    secret_name = "app/mysql/credentials"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    loop = asyncio.get_running_loop()
+
+    try:
+        # Unpack the dictionary into keyword arguments
+        get_secret_value_response = await loop.run_in_executor(
+            None,  # Uses the default executor
+            lambda: client.get_secret_value(SecretId=secret_name)
+        )
+    except ClientError as e:
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    return eval(secret)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+# Create a CloudWatch log client
+try:
+    log_client = boto3.client('logs', region_name="us-east-1")
+except NoCredentialsError:
+    logger.error("AWS credentials not found")
+
+LOG_GROUP = '/my-fastapi-app/logs'
+LOG_STREAM = os.getenv("INSTANCE_ID")
+
+# Function to push logs to CloudWatch
+import asyncio
+
+
+async def push_logs_to_cloudwatch(log_message):
+    loop = asyncio.get_running_loop()
+    try:
+        await loop.run_in_executor(
+            None,  # Uses the default executor (which is a ThreadPoolExecutor)
+            lambda: log_client.put_log_events(
+                logGroupName=LOG_GROUP,
+                logStreamName=LOG_STREAM,
+                logEvents=[
+                    {
+                        'timestamp': int(round(time.time() * 1000)),
+                        'message': log_message
+                    },
+                ],
+            )
+        )
+    except Exception as e:
+        logger.error(f"Error sending logs to CloudWatch: {e}")
+
 class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer, primary_key=True, index=True)
@@ -29,47 +97,67 @@ SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(bind=engine)
 print("Database initialized")
 @app.post("/items/")
-def create_item(name: str, description: str):
+async def create_item(name: str, description: str):
     db = SessionLocal()
     new_item = Item(name=name, description=description)
     db.add(new_item)
     db.commit()
     db.refresh(new_item)
+    await push_logs_to_cloudwatch(f"Item {new_item.id} created")
     return new_item
 
 @app.get("/items/")
-def read_items():
+async def read_items():
     db = SessionLocal()
     items = db.query(Item).all()
+    
+    await push_logs_to_cloudwatch(f"Get all items. Total items: {len(items)}")
+    
     return items
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int):
+async def read_item(item_id: int):
     db = SessionLocal()
     item = db.query(Item).filter(Item.id == item_id).first()
     if item is None:
+        await push_logs_to_cloudwatch(f"Item {item_id} not found")
         raise HTTPException(status_code=404, detail="Item not found")
+    
+    await push_logs_to_cloudwatch(f"Item {item_id} retrieved")
+    
     return item
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, name: str, description: str):
+async def update_item(item_id: int, name: str, description: str):
     db = SessionLocal()
     item = db.query(Item).filter(Item.id == item_id).first()
     if item is None:
+        
+        await push_logs_to_cloudwatch(f"Item {item_id} not found")
+        
         raise HTTPException(status_code=404, detail="Item not found")
 
     item.name = name
     item.description = description
     db.commit()
+    
+    await push_logs_to_cloudwatch(f"item {item_id} updated")
+    
     return item
 
 @app.delete("/items/{item_id}")
-def delete_item(item_id: int):
+async def delete_item(item_id: int):
     db = SessionLocal()
     item = db.query(Item).filter(Item.id == item_id).first()
     if item is None:
+        
+        await push_logs_to_cloudwatch(f"Item {item_id} not found")
+        
         raise HTTPException(status_code=404, detail="Item not found")
 
     db.delete(item)
     db.commit()
+    
+    await push_logs_to_cloudwatch(f"Item {item_id} deleted")
+    
     return {"detail": "Item deleted successfully"}
